f2f7431ff150a811a51119d5a707313e26a843b0
Song <786219380@qq.com>
2022-11-25 20:36:57 +0800

    111

--- app/src/main/java/com/chenwenrao/cdp/MainActivity.java
+++ app/src/main/java/com/chenwenrao/cdp/MainActivity.java
@@ -3,12 +3,23 @@ package com.chenwenrao.cdp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button=(Button) findViewById(R.id.bnt1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"我是小陈",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
